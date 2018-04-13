a9bbbee88424dd075ae52a5b9d8b9f0d39f0471d
Japa <japa.mala.illo@gmail.com>
2018-04-13 21:03:07 +0530

    Basic setup

---
+++ FortressToMinecraftConverter/ColorConsoleStream.cs
@@ -0,0 +1,91 @@
+﻿using AT.MIN;
+using DFHack;
+using System;
+
+namespace FortressToMinecraftConverter
+{
+    class ColorConsoleStream : IDFStream
+    {
+        public void AddText(ColorValue color, string text)
+        {
+            switch (color)
+            {
+                case ColorValue.ColorReset:
+                    Console.ResetColor();
+                    break;
+                case ColorValue.ColorBlack:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case ColorValue.ColorBlue:
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case ColorValue.ColorGreen:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case ColorValue.ColorCyan:
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case ColorValue.ColorRed:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case ColorValue.ColorMagenta:
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case ColorValue.ColorBrown:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case ColorValue.ColorGrey:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case ColorValue.ColorDarkgrey:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case ColorValue.ColorLightblue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case ColorValue.ColorLightgreen:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case ColorValue.ColorLightcyan:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case ColorValue.ColorLightred:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case ColorValue.ColorLightmagenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case ColorValue.ColorYellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case ColorValue.ColorWhite:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    break;
+            }
+            Console.Write(text);
+        }
+
+        public void BeginBatch()
+        {
+        }
+
+        public void EndBatch()
+        {
+            Console.Write(Environment.NewLine);
+        }
+
+        public void Print(string format, params object[] parameters)
+        {
+            Console.WriteLine(Tools.Sprintf(format, parameters).TrimEnd('\r', '\n'));
+        }
+
+        public void PrintErr(string format, params object[] parameters)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Tools.Sprintf(format, parameters).TrimEnd('\r', '\n'));
+            Console.ResetColor();
+        }
+    }
+}
---
+++ FortressToMinecraftConverter/MapReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFHack;
+
+namespace FortressToMinecraftConverter
+{
+    class MapReader
+    {
+        public bool ConnectToDF()
+        {
+            ColorConsoleStream stream = new ColorConsoleStream();
+            RemoteClient client = new RemoteClient(stream);
+            if(!client.Connect())
+            {
+                return false;
+            }
+
+
+
+            return true;
+            
+        }
+    }
+}
